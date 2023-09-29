08686bd77a676bfa412b663964ba8e9717d9c2c0
abdulrehman75 <abdullrehman.te@gmail.com>
2023-09-29 20:26:49 +0500

    first commit of websocket server

---
+++ server.js
@@ -0,0 +1,35 @@
+import { WebSocketServer } from 'ws';
+import axios from 'axios';
+const wss = new WebSocketServer({ port: 8080 });
+
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', async function message(data) {
+        try {
+            const receivedData = JSON.parse(data);
+
+            const apiResponse = await makeApiCall(receivedData);
+
+            ws.send(JSON.stringify(apiResponse));
+
+            // Perform operations with the received data
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+        }
+    });
+});
+
+async function makeApiCall(data) {
+    try {
+        // Make an API call using axios or another HTTP library
+        const response = await axios.post('http://16.170.201.181:5001/api/send-message', data);
+
+        // Return the API response data
+        return response.data;
+    } catch (error) {
+        // Handle API call errors here
+        console.error('API Error:', error);
+        return { error: 'API call failed' };
+    }
+}
+
