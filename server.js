abe82ef0e415e04938dd2a5bcc9106ae747cc15a
abdulrehman75 <abdullrehman.te@gmail.com>
2023-09-29 20:54:41 +0500

    add log

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import { WebSocketServer } from 'ws';
 import axios from 'axios';
 const wss = new WebSocketServer({ port: 8080 });
 
+console.log('websocket ready on port 8080');
+
 
 wss.on('connection', function connection(ws) {
     ws.on('message', async function message(data) {
